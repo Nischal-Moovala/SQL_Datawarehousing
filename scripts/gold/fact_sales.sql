6b9b243c05beef4c2483d8441e45d9d99f92f56f
Nischal-Moovala <121452228+Nischal-Moovala@users.noreply.github.com>
2025-06-22 19:48:12 +0530

    Create fact_sales.sql

---
+++ scripts/gold/fact_sales.sql
@@ -0,0 +1,40 @@
+/*
+===============================================================================
+DDL Script: Create Gold Views
+===============================================================================
+Script Purpose:
+    This script creates views for the Gold layer in the data warehouse. 
+    The Gold layer represents the final dimension and fact tables (Star Schema)
+
+    Each view performs transformations and combines data from the Silver layer 
+    to produce a clean, enriched, and business-ready dataset.
+
+Usage:
+    - These views can be queried directly for analytics and reporting.
+===============================================================================
+*/
+
+-- =============================================================================
+-- Create Fact Table: gold.fact_sales
+-- =============================================================================
+
+
+IF OBJECT_ID('gold.fact_sales', 'V') IS NOT NULL
+    DROP VIEW gold.fact_sales;
+GO
+CREATE VIEW gold.fact_sales AS
+SELECT
+    sd.sls_ord_num  AS order_number,
+    pr.product_key  AS product_key,
+    cu.customer_key AS customer_key,
+    sd.sls_order_dt AS order_date,
+    sd.sls_ship_dt  AS shipping_date,
+    sd.sls_due_dt   AS due_date,
+    sd.sls_sales    AS sales_amount,
+    sd.sls_quantity AS quantity,
+    sd.sls_price    AS price
+FROM silver.crm_sales_details sd
+LEFT JOIN gold.dim_products pr
+    ON sd.sls_prd_key = pr.product_number
+LEFT JOIN gold.dim_customers cu
+    ON sd.sls_cust_id = cu.customer_id;
